a30a54b8066a24b94e45b95eca58fd813ccbf570
Artsiom Trubchyk <aglitchman@gmail.com>
2023-10-11 17:20:31 +0300

    Add scale mode for noise texture and upgrade to Defold 1.6.1

--- dissolve_fx/dissolve_fx.lua
+++ dissolve_fx/dissolve_fx.lua
@@ -8,7 +8,7 @@
 -- local dissolve_fx = require("dissolve_fx.dissolve_fx")
 -- 
 -- function init(self)
---     dissolve_fx.init("#sprite", "noise", "ramp") -- args: sprite component with dissolve material, noise image name, ramp image name.
+--     dissolve_fx.init("#sprite", "noise", "ramp") -- args: sprite component with dissolve material, noise image name, ramp image name, scale mode (fit or stretch).
 --     go.set("#sprite", "dissolve.z", 0.25) -- size of the fire. Adjust subjectively to your eye!
 --     go.set("#sprite", "dissolve.w", 0.0) -- to control the fx, 0.0 - 1.0
 -- 
@@ -19,7 +19,7 @@
 
 local M = {}
 
-function M.find_uvrect(sprite_url, image_id)
+function M.find_uvrect(sprite_url, image_id, scale_mode)
     assert(type(image_id) == "string", "`image_id` should be a string")
     local atlas = go.get(sprite_url, "image")
     local atlas_data = resource.get_atlas(atlas)
@@ -51,14 +51,37 @@ function M.find_uvrect(sprite_url, image_id)
         height = uvs[6] - uvs[2]
     end
 
+    local w = width / tex_w
+    local h = height / tex_h
+    if scale_mode then
+        assert(scale_mode == "stretch" or scale_mode == "cover", "`scale_mode` should be `cover` or `stretch`")
+        -- When the `scale_mode` argument is specified, w and h will be divided by the size of the sprite.
+        -- This operation is needed to calculate the uv for the noise texture from the local positions
+        -- in the vertex shader.
+        local sprite_image_id = go.get(sprite_url, "animation")
+        for _, animation in ipairs(atlas_data.animations) do
+            if hash(animation.id) == sprite_image_id then
+                if scale_mode == "stretch" then
+                    w = w / animation.width
+                    h = h / animation.height
+                else -- cover
+                    local max = math.max(animation.width, animation.height)
+                    w = w / max
+                    h = h / max
+                end
+                break
+            end
+        end
+    end
+
     return (position_x + width/2) / tex_w,         -- x + w/2
         (tex_h - (position_y + height/2)) / tex_h, -- y + h/2
-        width / tex_w,                             -- w
-        height / tex_h                             -- h
+        w,                                         -- w
+        h                                          -- h
 end
 
-function M.init(sprite_url, noise_image, ramp_image)
-    go.set(sprite_url, "noise_uvrect", vmath.vector4(M.find_uvrect(sprite_url, noise_image)))
+function M.init(sprite_url, noise_image, ramp_image, noise_scale_mode)
+    go.set(sprite_url, "noise_uvrect", vmath.vector4(M.find_uvrect(sprite_url, noise_image, noise_scale_mode or "stretch")))
     go.set(sprite_url, "ramp_uvrect", vmath.vector4(M.find_uvrect(sprite_url, ramp_image)))
 end
 
